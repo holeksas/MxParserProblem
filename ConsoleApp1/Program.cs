c674c571c6720676e990cc20cec2e807ba949598
Szymon Holeksa <szymon.holeksa@rnt.eu>
2018-07-18 12:13:06 +0200

    NEW: Example

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using Logic;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new Class1();
+            c.Calc("2+2*2");
+            var d = c.Calc("2+2*2");
+            Console.WriteLine($"2+2*2 = {d}");
+        }
+    }
+}
---
+++ Logic/Class1.cs
@@ -0,0 +1,15 @@
+﻿//using Newtonsoft.Json.Linq;
+using org.mariuszgromada.math.mxparser;
+using System;
+
+namespace Logic
+{
+    public class Class1
+    {
+        public double Calc(string expr)
+        {
+            var ex = new Expression(expr);
+            return ex.calculate();
+        }
+    }
+}
